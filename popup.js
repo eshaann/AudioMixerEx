b2d11597267498cab91780f1c5eb6f4be5ff25fc
TJ-Hacker <55298280+TJ-Hacker@users.noreply.github.com>
2022-05-18 10:33:57 -0400

    Automate adding sliders
    not sure if it works, just added the code so I can test it at home

--- popup.js
+++ popup.js
@@ -1,14 +1,28 @@
-var tabs = chrome.tabs.query({"currentWindow":true, "audible":true}, updateTabList);
+let tabs = chrome.tabs.query({"currentWindow":true}, updateTabList);
+var tabList;
 
 function updateTabList (tabList) {
     for (i = 0; i < tabList.length; i ++) {
-        addListItem(tabList[i]);
+        addListItem(tabList[i], i);
     }
+    tabList = this.tabList;
 }
 
-function addListItem (tab) {
-    list = document.getElementById("list");
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(tab.index));
-    list.appendChild(li);
+function addListItem (tab, number) {
+    listContainer = document.getElementById("sliders");
+    var divToAdd = document.createElement("div");
+    divToAdd.setAttribute("id", "slider_" + toString(i));
+
+    listContainer.appendChild(divToAdd);
+
+    let name = "slider_" + toString(i);
+
+    var slider = document.createElement("input");
+    slider.setAttribute("type", "slider");
+    slider.setAttribute("min", "1");
+    slider.setAttribute("max", "100");
+    slider.setAttribute("value", "50");
+    slider.setAttribute("class", "slider");
+
+    document.getElementById(name).appendChild(slider);
 }
