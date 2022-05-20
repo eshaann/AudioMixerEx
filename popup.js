37ccceb43a5cdd6cabc670c482c9e6907917e14b
Tanvir Jawad <tjawad6912@bths.edu>
2022-05-20 10:54:05 -0400

    fixed a lotta shit

--- popup.js
+++ popup.js
@@ -7,21 +7,20 @@ function updateSliders () {
     for (i = 0; i < Object.keys(tabList).length; i ++) {
         listContainer = document.getElementById("sliders");
         var divToAdd = document.createElement("div");
-        divToAdd.setAttribute("id", "slider_" + toString(tabList[tabList.keys()[i]]));
+        divToAdd.setAttribute("id", "slider_" + toString(tabList[Object.keys(tabList)[i]]));
 
         listContainer.appendChild(divToAdd);
 
-        let name = "slider_" + toString(tabList[tabList.keys()[i]]);
+        let name = "slider_" + toString(tabList[Object.keys(tabList)[i]]);
 
         // Create A Slider For Each Audible Tab
         var slider = document.createElement("input");
-        slider.setAttribute("type", "slider");
+        slider.setAttribute("type", "range");
         slider.setAttribute("min", "1");
         slider.setAttribute("max", "100");
         slider.setAttribute("value", "50");
         slider.setAttribute("class", "slider");
 
-        tabList.push("")
 
         document.getElementById(name).appendChild(slider);
     }
@@ -31,14 +30,14 @@ function updateTabList (inpList) {
     console.log("Orig keys: " + Object.keys(tabList));
     for (i = 0; i < inpList.length; i ++) {
         console.log("ID: " + inpList[i].id);
-        if (Object.keys(inpList).indexOf(toString(inpList[i].id)) < 0) {
-            inpList.push({
-                key: toString(inpList[i].id),
-                value: 100
-            });
-        }
+        // if (Object.keys(inpList).indexOf(toString(inpList[i].id)) < 0) {
+            
+        // }
+
+        tabList[toString(inpList[i].id)] = 100;
 
     }
+
     console.log("Final keys: " + Object.keys(tabList));
 
     updateSliders();
